99ab51f37189bc4191efe9cd43bdf949030ff084
Shakrd <soulofshaka@gmail.com>
2020-03-06 11:08:11 -0500

    se agregaron objetivos y descripcion a la entidad asignatura

--- src/app/mocks/mock-prerrequisitosAsignatura.ts
+++ src/app/mocks/mock-prerrequisitosAsignatura.ts
@@ -1,8 +1,8 @@
 import { Asignatura } from "../entidades/asignatura";
 
 export const PRERREQUISITOASIGNATURAS: Asignatura[] = [
-    {id:1, nombre: 'Base de Datos', codigo: 'abc1234'},
-    {id:2, nombre: 'Ingeniería de Software', codigo: 'qwer789'},
-    {id:3, nombre: 'Procesos', codigo: 'pjyge8925'},    
-    {id:4, nombre: 'Tendencias de Programación', codigo: 'azxc463'}
+    {id:1, nombre: 'Base de Datos', codigo: 'abc1234', descripcion: 'Aqui se ingresara la descripcion del la asignatura',objetivo: 'Aqui se ingresara el objetivo'},
+    {id:2, nombre: 'Ingeniería de Software', codigo: 'qwer789', descripcion: 'Aqui se ingresara la descripcion del la asignatura',objetivo: 'Aqui se ingresara el objetivo'},
+    {id:3, nombre: 'Procesos', codigo: 'pjyge8925', descripcion: 'Aqui se ingresara la descripcion del la asignatura',objetivo: 'Aqui se ingresara el objetivo'},    
+    {id:4, nombre: 'Tendencias de Programación', codigo: 'azxc463', descripcion: 'Aqui se ingresara la descripcion del la asignatura',objetivo: 'Aqui se ingresara el objetivo'}
 ];
