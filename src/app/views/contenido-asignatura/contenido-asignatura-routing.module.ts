132265b3f797c7222404d5bdfb03da21b8ea9281
jeanstkng <jjg.castillo@yavirac.edu.ec>
2020-04-17 14:16:26 -0500

    contenido asignatura modificacion y correcciones

--- src/app/views/contenido-asignatura/contenido-asignatura-routing.module.ts
+++ src/app/views/contenido-asignatura/contenido-asignatura-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
     path: '',
     component: ContenidoAsignaturaComponent,
     data: {
-      title: 'ContenidoAsignatura'
+      title: 'Contenido Asignatura'
     }
   }
 ];
